4ab2aaa42927cefea08ec8ad100860a1d168bc48
6453Zafir <511599737@qq.com>
2017-05-06 17:15:10 +0800

    alert made

--- js/Layout.js
+++ js/Layout.js
@@ -1,10 +1,31 @@
 /**
  * Created by admin on 2017/5/4.
  */
-$("#canvasArea").load("FullScreenLayout.html");
-$("#template1").on("click", function() {
+function layoutController() {
     $("#canvasArea").load("FullScreenLayout.html");
-})
-$("#template0").on("click", function() {
-    $("#canvasArea").load("BasicLayout.html");
-})
+    $("#template1").on("click", function() {
+        LayoutNum=1;
+        $('#myModal').modal('hide');
+        // $("#canvasArea").load("FullScreenLayout.html");
+    })
+    $("#template2").on("click", function() {
+        LayoutNum=2;
+        $('#myModal').modal('hide');
+        // $("#canvasArea").load("BasicLayout.html");
+    })
+    $("#template3").on("click", function() {
+        LayoutNum=3;
+        $('#myModal').modal('hide');
+        // $("#canvasArea").load("BasicLayout.html");
+    })
+}
+
+function basicLayout() {
+    
+}
+function FullLayout() {
+
+}
+function mapLayout() {
+
+}
