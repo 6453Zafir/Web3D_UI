208623092137883728a3568a1ab54a907f0f5145
6453Zafir <511599737@qq.com>
2017-05-15 20:52:56 +0800

    point light accomplished

--- js/Light-controller.js
+++ js/Light-controller.js
@@ -10,13 +10,17 @@ function lightController() {
     if(LayoutNum==0){
         console.log("haven't choose template yet")
     }else {
+        $("#point-light").on("click", function () {
+            initPointLight();
+            LightNum = 1;
+        })
         $("#directional-light").on("click", function () {
             initDirectionalLight();
             LightNum = 3;
         })
-        $("#point-light").on("click", function () {
-            initPointLight();
-            LightNum = 1;
+        $("#area-light").on("click", function () {
+            initAreaLight();
+            LightNum = 4;
         })
     }
 }
--- js/Light.js
+++ js/Light.js
@@ -10,9 +10,11 @@ var AreaLightGui,PointLightGui,DirectionalLightGui,SpotLightGui;
 var lightControls = new Array(pointLightControl,spotLightControl,directionalLightControl,areaLightControl);
 var lightGuis = new Array(PointLightGui,SpotLightGui,DirectionalLightGui,AreaLightGui);
 
+var moveAreaLight = true;
+var movePointLight = true;
+var moveSpotLight = true;
 
-
-function initDirectionalLight() {
+function initAreaLight() {
     var matParams = {
         specular: 0xFFFFFF,
         shininess: 10000
@@ -20,25 +22,13 @@ function initDirectionalLight() {
 
     var amb = new THREE.AmbientLight( 0x080808 );
 
-    // TODO (abelnation): temp point light for debugging
-    var dir = new THREE.DirectionalLight( 0xFFFFFF );
-    var dirHelper = new THREE.DirectionalLightHelper( dir );
-
-    var shapes, shapeNames;
     var rectLight;
     var rectLightHelper;
-
-    var AreaLightGui;
-    var param = {};
-
-    var moveAreaLight = true;
 
     function init() {
 
         var gl = renderer.context;
 
-        // Check for float-RT support
-        // TODO (abelnation): figure out fall-back for float textures
         if (!gl.getExtension("OES_texture_float")) {
             alert("OES_texture_float not supported");
             throw "missing webgl extension";
@@ -55,7 +45,6 @@ function initDirectionalLight() {
         renderer.gammaOutput = true;
         renderer.antialias = true;
 
-
         rectLight = new THREE.RectAreaLight( 0xFFFFFF, undefined, 10, 10 );
         rectLight.matrixAutoUpdate = true;
         rectLight.intensity = 100.0;
@@ -63,22 +52,12 @@ function initDirectionalLight() {
         rectLightHelper = new THREE.RectAreaLightHelper( rectLight );
         rectLight.add( rectLightHelper );
 
-        // TODO (abelnation): rect light shadow
-
-
-        // matFloor.color.set( 0x808080 );
         randomColor( cube );
-
-        // mshFloor.receiveShadow = true;
-        // mshFloor.position.set( 0, 0, 0 );
 
         cube.castShadow = true;
         cube.receiveShadow = true;
 
-        // scene.add( mshFloor );
-
         scene.add( rectLight );
-
     }
 
     function tick() {
@@ -90,7 +69,6 @@ function initDirectionalLight() {
     }
 
     function update() {
-
         var dt = 6000;
         var qdt = dt / 4.0;
         var dirSigns = [
@@ -101,16 +79,12 @@ function initDirectionalLight() {
         ];
         var t = ( Date.now() / 1000 );
 
-        // move light in circle around center
-        // change light height with sine curve
-
         var r = 15.0;
 
         var lx = r * Math.cos( t );
         var lz = r * Math.sin( t );
 
         var ly = 5.0 + 5.0 * Math.sin( t / 3.0 );
-        // var ly = 7.0;
 
         rectLight.position.set( lx, ly, lz );
         rectLight.lookAt( cube.position );
@@ -118,10 +92,8 @@ function initDirectionalLight() {
     }
 
     function render() {
-
         rectLightHelper.update(); // required
         renderer.render( scene, camera );
-
     }
 
     function clearGui() {
@@ -142,9 +114,7 @@ function initDirectionalLight() {
 
         clearGui();
 
-
         //--------control the position/rotation/range by mouse drag---------
-
         lightControls.areaLightControl= new THREE.TransformControls(camera,renderer.domElement);
         lightControls.areaLightControl.attach( rectLight );
         lightControls.areaLightControl.addEventListener( 'change', function () {
@@ -207,9 +177,7 @@ function initDirectionalLight() {
 
             }
         }
-
     }
-
     init();
     buildGui();
     tick();
@@ -218,63 +186,93 @@ function initDirectionalLight() {
 
 function initPointLight(){
     var clock = new THREE.Clock();
-    var light1, light2, light3, light4;
+    var pointlight;
     var sphere = new THREE.SphereGeometry( 0.5, 16, 8 );
+    var pointlightHelper = new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) );
 
-    light1 = new THREE.PointLight( 0xff0040, 2, 50 );
-    light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
-    light1.position.set(-20,15,10);
-    scene.add( light1 );
+    pointlight = new THREE.PointLight( 0xff0040, 6, 100,1 );
+    pointlight.add( pointlightHelper);
+    pointlight.position.set(-20,15,10);
+    scene.add( pointlight );
 
-    light2 = new THREE.PointLight( 0x0040ff, 2, 50 );
-    light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
-    light2.position.set(20,15,10);
-    scene.add( light2 );
+    //--------control the position/rotation/range by mouse drag---------
+    lightControls.pointLightControl= new THREE.TransformControls(camera,renderer.domElement);
+    lightControls.pointLightControl.attach( pointlight );
+    lightControls.pointLightControl.addEventListener( 'change', function () {
+        renderer.render( scene, camera );
+    } );
+    scene.add( lightControls.pointLightControl );
+    //--------control the position/rotation/range by mouse gui end---------
 
-    light3 = new THREE.PointLight( 0x80ff80, 2, 50 );
-    light3.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
-    light3.position.set(-20,-15,10);
-    scene.add( light3 );
+    if ( lightGuis.PointLightGui ) lightGuis.PointLightGui.destroy();
 
-    light4 = new THREE.PointLight( 0xffaa00, 2, 50 );
-    light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
-    light4.position.set(20,-15,10);
-    scene.add( light4 );
+    lightGuis.PointLightGui = new dat.GUI();
+
+    $(".dg.ac").appendTo("#moduleArea");
+    $(".dg.ac").css("position","absolute");
+    $(".dg.ac").css("top","15px");
+
+    lightGuis. PointLightGui.open();
+
+    var pointLightData = function(){
+        this.color=pointlight.color.getHex();
+        this.intensity=pointlight.intensity;
+        this.distence = pointlight.distance;
+        this.decay = pointlight.decay;
+        this.visible = true;
+        this.move = true;
+        this.position = function () {lightControls.pointLightControl.setMode("translate")};
+        this.rotation = function () {lightControls.pointLightControl.setMode("rotate")};
+        this.scale = function () {lightControls.pointLightControl.setMode("scale")};
+        this.delete = function () {
+            scene.remove(pointlight);
+            lightGuis.PointLightGui.hide();
+            scene.remove(lightControls.pointLightControl);
+        };
+    }
+
+    var param = new pointLightData();
+    lightGuis.PointLightGui.addColor( param, 'color' ).onChange( function ( val ) {pointlight.color.setHex( val );} );
+    lightGuis.PointLightGui.add( param, 'intensity', 0,50 ).onChange( function ( val ) {pointlight.intensity = val;} );
+    lightGuis.PointLightGui.add( param, 'distence', 1, 200).onChange( function ( val ) {pointlight.distence = val;} );
+    lightGuis.PointLightGui.add( param, 'decay', 1, 2).onChange( function ( val ) {pointlight.decay = val;} );
+    lightGuis.PointLightGui.add( param, 'visible' ).onChange( function ( val ) {
+        if(val == false){
+            pointlightHelper.visible = false;
+        }else{
+            pointlightHelper.visible = true;
+        }
+    } );
+    lightGuis.PointLightGui.add( param, 'move').onChange( function ( val ) {
+        movePointLight = val;
+    } );
+
+    var transform = lightGuis.PointLightGui.addFolder('Transform');
+    transform.add(param, 'position');
+    transform.add(param, 'rotation');
+    transform.add(param, 'scale');
+
+    lightGuis.PointLightGui.add(param,'delete');
 
     renderer.render(scene,camera);
+
     animate();
     function animate() {
-
         requestAnimationFrame( animate );
-
-        // render();
+        if(movePointLight){
+            update();
+        }
         renderer.render( scene, camera );
     }
 
-    function render() {
+    function update() {
 
-        // var time = Date.now() * 0.0005;
-        // var delta = clock.getDelta();
-        //
-        //
-        // light1.position.x = Math.sin( time * 0.7 ) * 30;
-        // light1.position.y = Math.cos( time * 0.5 ) * 40;
-        // light1.position.z = Math.cos( time * 0.3 ) * 30;
-        //
-        // light2.position.x = Math.cos( time * 0.3 ) * 30;
-        // light2.position.y = Math.sin( time * 0.5 ) * 40;
-        // light2.position.z = Math.sin( time * 0.7 ) * 30;
-        //
-        // light3.position.x = Math.sin( time * 0.7 ) * 30;
-        // light3.position.y = Math.cos( time * 0.3 ) * 40;
-        // light3.position.z = Math.sin( time * 0.5 ) * 30;
-        //
-        // light4.position.x = Math.sin( time * 0.3 ) * 30;
-        // light4.position.y = Math.cos( time * 0.7 ) * 40;
-        // light4.position.z = Math.sin( time * 0.5 ) * 30;
+        var time = Date.now() * 0.0005;
+        var delta = clock.getDelta();
 
-        // renderer.render( scene, camera );
-
+        pointlight.position.x = Math.sin( time * 0.7 ) * 20;
+        pointlight.position.y = Math.cos( time * 0.5 ) * 20;
+        pointlight.position.z = Math.cos( time * 0.3 ) * 20;
 
     }
 }
